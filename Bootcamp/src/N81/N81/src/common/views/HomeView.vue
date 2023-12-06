430ab6b9f1aac0f840acf4274910a21a4ad55425
Botirjon <thedavlativich@gmail.com>
2023-12-07 03:59:55 +0500

    update

---
+++ Bootcamp/src/N81/N81/src/common/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+
+  <article class="mt-[20px] bg-defaultBackground content-padding">
+    <div class="flex flex-col items-center justify-center">
+
+    </div>
+    
+  </article>
+
+  <locations-grid/>
+
+</template>
+<script setup lang="ts">
+import LocationsGrid from "@/modules/locations/components/LocationsGrid.vue";
+</script>
---
+++ Bootcamp/src/N81/N81/src/modules/locations/components/LocationsGrid.vue
@@ -0,0 +1,7 @@
+<template>
+
+  <div class="mt-[160px] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-x-4 gap-y-10 place-items-center">
+
+  </div>
+
+</template>
