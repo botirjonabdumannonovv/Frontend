af06bb6d8efba1d82c3b554831ee300680ab123b
Botirjon <thedavlativich@gmail.com>
2023-12-08 12:05:30 +0500

    add homeview

--- Bootcamp/src/N81/N81/src/App.vue
+++ Bootcamp/src/N81/N81/src/App.vue
@@ -1,9 +1,9 @@
 <template>
 
-  <main-logo/>
+  <home-view/>
 
 </template>
 
 <script setup lang="ts">
-import MainLogo from "@/common/components/icons/MainLogo.vue";
+import HomeView from "@/common/views/HomeView.vue";
 </script>
